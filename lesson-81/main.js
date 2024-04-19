17f3655349e3aa6273b69a7c1d0fe8ef96b16344
angeliiina0-0 <163559121+angeliiina0-0@users.noreply.github.com>
2024-04-19 23:09:09 +0300

    add lesson-81

---
+++ lesson-81/main.js
@@ -0,0 +1,16 @@
+// Задание 1
+// const numbers = [2, 6, 3, 72, 9, 21, 100]
+// for(let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//         console.log(numbers[i])
+//     }
+// }
+
+
+
+// Задание 2
+const colorRainbow = ['красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый']
+for (let i = 6; i != -1 ; i--) {
+    console.log(colorRainbow[i])
+}
+// не уверена, что это правильное решение...
