4bbc890ed03d3bf0a17afd9015519d6c87a197ad
angeliiina0-0 <163559121+angeliiina0-0@users.noreply.github.com>
2024-04-19 18:32:18 +0300

    add lesson-80

---
+++ lesson-80/main.js
@@ -0,0 +1,30 @@
+// Задание 1
+// for (let i = 2; i <= 12; i += 2) {
+//     console.log(i)
+// }
+
+
+
+// Задание 2
+// for(let i = 10; i >= 1; i -= 1) {
+//     console.log(i)
+// }
+
+
+
+// Задание 3
+    // Цикл 1
+// let i = 0
+
+// while(i < 12) {
+//     i += 2
+//     console.log(i)
+// }
+
+    // Цикл 2
+// let i = 11 
+
+// while (i > 1) {
+//     i -= 1
+//     console.log(i)
+// }
