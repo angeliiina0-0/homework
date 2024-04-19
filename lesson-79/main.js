3bb1776229b6c913bcfa0c1ec24ca07bae5d4469
angeliiina0-0 <163559121+angeliiina0-0@users.noreply.github.com>
2024-04-19 17:36:40 +0300

    add lesson-79

---
+++ lesson-79/main.js
@@ -0,0 +1,8 @@
+let myAge = 18
+    
+// if (myAge >= 18) {
+//     console.log('Вы совершеннолетний')
+// }else {
+//     console.log('Вам еще нет 18')
+// }
+myAge >= 18 ? console.log('Вы совершеннолетний') : console.log('Вам еще нет 18')
