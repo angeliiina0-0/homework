5c88b903d2366fcb55a50c308492dc1815640b2e
angeliiina0-0 <163559121+angeliiina0-0@users.noreply.github.com>
2024-04-19 11:53:55 +0300

    add lesson-76

---
+++ lesson-76/main.js
@@ -0,0 +1,20 @@
+let firstName, lastName, myBirthday, myCity, messange;
+    firstName = 'Angelina';
+    lastName = 'Rybanova';
+    myBirthday = '14.01.2006';
+    myCity = 'Nizhniy Novgorod';
+    messange = 'Hello, my friends!';
+
+console.log(firstName, lastName)
+console.log(myBirthday)
+console.log(myCity)
+console.log(messange)
+
+
+
+
+
+
+
+
+
